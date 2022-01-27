8edf3f28f9fa0e242a523db595315a9ed06fe70c
Benedikt Müssig <github@bmuessig.eu>
2022-01-27 15:47:11 +0100

    Added the German translation

--- mb_ser/js/utils/lang.js
+++ mb_ser/js/utils/lang.js
@@ -9,6 +9,7 @@ import { trans_zh_hk }  from './trans/zh_hk.js'
 import { trans_zh_cn }  from './trans/zh_cn.js'
 import { trans_ja }     from './trans/ja.js'
 import { trans_ko }     from './trans/ko.js'
+import { trans_de }     from './trans/de.js'
 
 let trans = null;
 if (navigator.language.startsWith('zh')) {
@@ -20,6 +21,8 @@ if (navigator.language.includes('ja')) // Japanese
     trans = trans_ja;
 if (navigator.language.includes('ko')) // Korean
     trans = trans_ko;
+if (navigator.language.includes('de')) // German
+    trans = trans_de;
 
 function L(ori, mark=null) {
     if (trans == null)
